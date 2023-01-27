c8bf740b6187ce1072adb968b6458fe1d28425e8
Ioan23 <75176548+Ioan23@users.noreply.github.com>
2023-01-27 12:54:31 +0200

    Initial commit
    Initial

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Salut, Bine ai venit pe site");
+
+function count2Number(firstNr, secondNr){
+    var sum = firstNr+secondNr;
+    // const sums =  sum;
+    console.log(sum);
+
+}
+
+
+count2Number(5, 21);
